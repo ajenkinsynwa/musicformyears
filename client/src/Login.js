a818365d17cc56eaf06452dd3d738b6242c57181
alexjenkinsynwa@gmail.com <alexjenkins@Alexs-MacBook-Pro.local>
2022-11-10 12:56:33 -0500

    updated login

---
+++ client/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from "react-bootstrap"
+
+const AUTH_URL = "https://accounts.spotify.com/authozie?client_id=3ed3fe85267b476e85a0b6eb3661eff1&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+  return (
+    <div>
+
+    </div>
+  )
+}
